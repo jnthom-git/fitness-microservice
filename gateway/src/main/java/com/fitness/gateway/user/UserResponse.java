7f693ad2877eb2ebf2ea6da6af89db55b97ed8ee
Thomas <jmagtibay39@gmail.com>
2026-08-25 17:12:49 +0800

    Add Keycloak user synchronization filter and update user registration handling

---
+++ gateway/src/main/java/com/fitness/gateway/user/UserResponse.java
@@ -0,0 +1,18 @@
+package com.fitness.gateway.user;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserResponse {
+
+    private String id;
+    private String keycloakId;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
