360d5e48d4eaa52c67e19d75957c5cc9795152d3
Thomas <jmagtibay39@gmail.com>
2026-08-29 17:10:10 +0800

    Add CORS configuration and user ID handling in activity tracking

--- fitness-app-frontend/src/components/ActivityForm.jsx
+++ fitness-app-frontend/src/components/ActivityForm.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, duration, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React, { useState } from 'react';
+import { addActivity } from '../services/api';
 
 
 
@@ -13,6 +14,7 @@ const ActivityForm = ( {onActivityAdded} ) => {
         onActivityAdded();
         setActivity({ type: "RUNNING", duration: '', caloriesBurned: '' });
     } catch (error) {
+        console.error(error);
     }
 }
     return (
