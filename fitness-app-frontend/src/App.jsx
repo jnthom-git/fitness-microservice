fc608074ad294889a2c1014bde30049cc290c4d5
Thomas <jmagtibay39@gmail.com>
2026-08-29 15:35:32 +0800

    Add activity management components and routing to the app

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useContext, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "./store/authSlice";
 import { AuthContext } from "react-oauth2-code-pkce";
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail";
+import ActivityForm from "./components/ActivityForm";
+
+
+const ActivitiesPage = () => {
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      <ActivityForm onActivityAdded={() => window.location.reload()} />
+      <ActivityList />
+    </Box>
+  );
+}
 
 function App() {
 
@@ -26,9 +39,14 @@ function App() {
       <Button variant = "contained" color="#dc004e"
               onClick={() => logIn()}>login</Button>
       ) : (
-        <div>
-          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-        </div>
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+        <Routes>
+          <Route path="/activities" element={<ActivitiesPage />} />
+          <Route path="/activities/:id" element={<ActivityDetail />} />
+          
+          <Route path="/" element={token ? <Navigate to="/activities" replace/> : <div>Nothing to see here! Try logging in</div>} />
+        </Routes>
+    </Box>
       )}
     </Router>
   )
